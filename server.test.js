2fb5483f2337cf6606aa04b8a1a6f8899ae01a78
mar1n <30738216+mar1n@users.noreply.github.com>
2021-08-11 15:27:16 +0100

    testing with supertest

--- server.test.js
+++ server.test.js
@@ -3,9 +3,7 @@ const { app } = require("./server.js");
 const { addItemItemToCart, carts } = require("./CartController");
 const { inventory } = require("./InventoryController");
 
-const fetch = require("isomorphic-fetch");
-
-const apiRoot = "http://localhost:3000";
+const request = require("supertest");
 
 afterAll(() => app.close());
 
@@ -16,28 +14,28 @@ describe("Adding Items", () => {
   test("adding items", async () => {
     inventory.set("t-shirt", 1);
 
-    const response = await fetch(`${apiRoot}/carts/test_user/items/t-shirt`, {
-      method: "POST",
-    });
+    const response = await request(app).post(
+      "/carts/test_user/items/t-shirt"
+    );
 
     expect(response.status).toEqual(200);
-    expect(await response.json()).toEqual(true);
+
+    expect(response.body).toEqual(true);
     expect(inventory.get("t-shirt")).toEqual(0);
     expect(carts.get("test_user")).toEqual(["t-shirt"]);
   });
   test("adding items", async () => {
     inventory.set("t-shirt", 1);
-    const response = await fetch(`${apiRoot}/carts/test_user/items/t-shirt`, {
-      method: "POST",
-    });
+    const response = await request(app).post(
+      "/carts/test_user/items/t-shirt"
+    );
 
     expect(response.status).toEqual(200);
-    console.log("await", await response.json());
   });
   test("item doesn't exist", async () => {
-    const response = await fetch(`${apiRoot}/carts/test_user/items/t-shirt`, {
-      method: "POST",
-    });
+    const response = await request(app).post(
+      "/carts/test_user/items/t-shirt"
+    );
     expect(response.status).toEqual(400);
   });
 });
@@ -46,23 +44,22 @@ describe("deleting items", () => {
   test("delete item", async () => {
     carts.set("Szymon", ["Visa-123"]);
     console.log("carts", carts);
-    const response = await fetch(`${apiRoot}/carts/Szymon/items/Visa-123`, {
-      method: "DELETE",
-    });
+    const response = await request(app).delete(
+      "/carts/Szymon/items/Visa-123"
+    );
     expect(response.status).toEqual(200);
-    expect(await response.json()).toEqual([]);
+    expect(response.body).toEqual([]);
   });
 });
 
 describe("getting items", () => {
-    test("item is available", async() => {
-        carts.set("Szymon", ["Visa-XXX"]);
-        const response = await fetch(
-            `${apiRoot}/carts/Szymon/items`,
-            { method: "GET"}
-        );
+  test("item is available", async () => {
+    carts.set("Szymon", ["Visa-XXX"]);
+    const response = await request(app).get(
+      "/carts/Szymon/items"
+    );
 
-        expect(response.status).toEqual(200);
-        expect(await response.json()).toEqual(["Visa-XXX"]);
-    })
-})
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(["Visa-XXX"]);
+  });
+});
