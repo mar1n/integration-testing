a95f2e6851eed57f4620577c968306ce875da66f
mar1n <30738216+mar1n@users.noreply.github.com>
2021-11-07 21:27:52 +0000

    writing better assertion

--- frontEndTesting/domController.test.js
+++ frontEndTesting/domController.test.js
@@ -1,55 +1,49 @@
 const fs = require("fs");
-const initialHTML = fs.readFileSync("./index.html");
-
+const initialHtml = fs.readFileSync("./index.html");
 const { getByText, screen } = require("@testing-library/dom");
 
 const { updateItemList } = require("./domController");
 
 beforeEach(() => {
-  document.body.innerHTML = initialHTML;
+  document.body.innerHTML = initialHtml;
 });
+
 describe("updateItemList", () => {
   test("updates the DOM with the inventory items", () => {
     const inventory = {
       cheesecake: 5,
       "apple pie": 2,
-      "carrot cake": 6,
+      "carrot cake": 6
     };
-
     updateItemList(inventory);
+
     const itemList = document.getElementById("item-list");
     expect(itemList.childNodes).toHaveLength(3);
 
-    expect(getByText(itemList, "cheesecake - Quantity: 5")).toBeTruthy();
-    expect(getByText(itemList, "apple pie - Quantity: 2")).toBeTruthy();
-    expect(getByText(itemList, "carrot cake - Quantity: 6")).toBeTruthy();
+    expect(getByText(itemList, "cheesecake - Quantity: 5")).toBeInTheDocument();
+    expect(getByText(itemList, "apple pie - Quantity: 2")).toBeInTheDocument();
+    expect(
+      getByText(itemList, "carrot cake - Quantity: 6")
+    ).toBeInTheDocument();
+  });
+
+  test("highlighting in red elements whose quantity is below five", () => {
+    const inventory = { cheesecake: 5, "apple pie": 2, "carrot cake": 6 };
+    updateItemList(inventory);
+
+    expect(screen.getByText("apple pie - Quantity: 2")).toHaveStyle({
+      color: "red"
+    });
   });
 
   test("adding a paragraph indicating what was the update", () => {
     const inventory = { cheesecake: 5, "apple pie": 2 };
     updateItemList(inventory);
-    const paragraphs = Array.from(document.querySelectorAll("p"));
-    const updateParagraphs = paragraphs.filter((p) => {
-      return p.innerHTML.includes("The inventory has been updated");
-    });
 
-    expect(updateParagraphs).toHaveLength(1);
-    expect(screen.getByText(`The inventory has been updated - ${JSON.stringify(inventory)}`)).toBeTruthy();
+    expect(
+      screen.getByText(
+        `The inventory has been updated - ${JSON.stringify(inventory)}`
+      )
+    ).toBeTruthy();
   });
-
-  test("updates the DOM with the inventory items", () => {
-    const inventory = {
-        cheesecake: 5,
-        "apple pie": 2,
-        "carrot cake": 6
-    };
-    updateItemList(inventory);
-
-    const itemList = document.getElementById("item-list");
-    expect(itemList.childNodes).toHaveLength(3);
-
-    expect(getByText(itemList, "cheesecake - Quantity: 5", { selector: "li"})).toBeTruthy();
-    expect(getByText(itemList, "apple pie - Quantity: 2")).toBeTruthy();
-    expect(getByText(itemList, "carrot cake - Quantity: 6")).toBeTruthy();
-  })
 });
