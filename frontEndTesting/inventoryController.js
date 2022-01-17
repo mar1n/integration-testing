1cdad873ce87f4c3299b50521ad0bca785518236
mar1n <30738216+mar1n@users.noreply.github.com>
2022-01-17 22:09:53 +0000

    connected socket

--- frontEndTesting/inventoryController.js
+++ frontEndTesting/inventoryController.js
@@ -3,12 +3,16 @@ const data = { inventory: {} };
 const API_ADDR = "http://localhost:3000";
 
 const addItem = (itemName, quantity) => {
+  const { client } = require("./socket");
+
   const currentQuantity = data.inventory[itemName] || 0;
   data.inventory[itemName] = currentQuantity + quantity;
 
   fetch(`${API_ADDR}/inventory/${itemName}`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json",
+    "x-socket-client-id": client.id
+  },
     body: JSON.stringify({ quantity })
   });
 
