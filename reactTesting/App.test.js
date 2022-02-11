be0b1ae9966cc5727b30875bc70c448aa77e9100
mar1n <30738216+mar1n@users.noreply.github.com>
2022-02-11 20:01:59 +0000

    on click event test in React

--- reactTesting/App.test.js
+++ reactTesting/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { App } from "./App.jsx";
 import { render } from "react-dom";
 import { act } from "react-dom/test-utils";
-import { screen } from "@testing-library/dom";
+import { screen, fireEvent } from "@testing-library/dom";
 
 const root = document.createElement("div");
 document.body.appendChild(root);
@@ -11,5 +11,11 @@ test('renders the approprate header', () => {
     act(() => {
         render(<App />, root);
     });
-    expect(screen.getByText("Inventory Contents")).toBeTruthy();
+    expect(screen.getByText("Cheesecakes: 0")).toBeInTheDocument();
+
+    const addCheesecakeBtn = screen.getByText("Add Cheesecake");
+    act(() => {
+        fireEvent.click(addCheesecakeBtn);
+    });
+    expect(screen.getByText("Cheesecakes: 1")).toBeInTheDocument();
 });
