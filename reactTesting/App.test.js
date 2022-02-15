4aa6ef51e7b05212b50e8de3f71887902da3875b
mar1n <30738216+mar1n@users.noreply.github.com>
2022-02-15 11:35:38 +0000

    test app component and child components

--- reactTesting/App.test.js
+++ reactTesting/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { App } from "./App.jsx";
 import { render, fireEvent, waitFor } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
-import { screen } from "@testing-library/dom";
+import { screen, wait } from "@testing-library/dom";
 import nock from "nock";
 import { API_ADDR } from "./constants.js";
 
@@ -36,3 +36,35 @@ test("rendering the server's list of items", async () => {
   const listElement = document.querySelector("ul");
   expect(listElement.childElementCount).toBe(3);
 });
+
+test('updating the list of items with new items', async () => {
+    nock(API_ADDR)
+        .post("/inventory/cheesecake", JSON.stringify({ quantity: 6 }))
+        .reply(200);
+
+    const { getByText, getByPlaceholderText } = render(<App />);
+
+    await waitFor(() => {
+        const listElement = document.querySelector("ul");
+        expect(listElement.childElementCount).toBe(3);
+    });
+
+    fireEvent.change(getByPlaceholderText("Item name"), {
+        target: { value: "cheesecake"}
+    });
+    fireEvent.change(getByPlaceholderText("Quantity"), {
+        target: {value: "6"}
+    });
+
+    fireEvent.click(getByText("Add item"));
+
+    await waitFor(() => {
+        expect(getByText("cheesecake - Quantity: 8")).toBeInTheDocument();
+    });
+
+    const listElement = document.querySelector("ul");
+    expect(listElement.childElementCount).toBe(3);
+
+    expect(getByText("croissant - Quantity: 5")).toBeInTheDocument();
+    expect(getByText("macaroon - Quantity: 96")).toBeInTheDocument();
+})
