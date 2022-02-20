118639882db29f8e9c39251682947f0624161154
mar1n <30738216+mar1n@users.noreply.github.com>
2022-02-20 23:01:07 +0000

    storybook

---
+++ reactTesting/ItemList.stories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { withKnobs, object } from "@storybook/addon-knobs";
+import { ItemList } from "./ItemList.jsx";
+
+export default {
+    title: "ItemList",
+    component: ItemList,
+    includeStories: ["staticItemList", "animatedItems"],
+    decorators: [withKnobs]
+};
+
+export const staticItemList = () => <ItemList itemList={{cheesecake: 2, croissant: 5, macaroon: 96 }} />
+export const animatedItems = () => {
+    const knobLabel = "Contents";
+    const knobDefaultValue = { cheesecake: 2, croissant: 5};
+    const itemList = object(knobLabel, knobDefaultValue);
+    return <ItemList itemList={itemList} />
+}
