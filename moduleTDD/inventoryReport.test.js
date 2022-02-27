2a8c3e38a08f834719097cb53d63522adb61dcad
mar1n <30738216+mar1n@users.noreply.github.com>
2022-02-27 15:20:14 +0000

    write inventory report

--- moduleTDD/inventoryReport.test.js
+++ moduleTDD/inventoryReport.test.js
@@ -1,22 +1,70 @@
-const { generateItemRow } = require("./inventoryReport");
+const {
+  generateItemRow,
+  generateTotalRow,
+  createInventoryValuesReport,
+} = require("./inventoryReport");
 
-test("generating an items row", () => {
-  const item = { name: "macaroon", quantity: 12, price: 3 };
-  expect(generateItemRow(item)).toBe("macaroon,12,3,36");
-  expect(generateItemRow({ name: "cheesecake", quantity: 6, price: 12 })).toBe(
-    "cheesecake,6,12,72"
-  );
-  expect(generateItemRow({ name: "apple pie", quantity: 5, price: 15 })).toBe(
-    "apple pie,5,15,75"
-  );
+describe("generateItemRow", () => {
+  test("generating an items row", () => {
+    const item = { name: "macaroon", quantity: 12, price: 3 };
+    expect(generateItemRow(item)).toBe("macaroon,12,3,36");
+    expect(
+      generateItemRow({ name: "cheesecake", quantity: 6, price: 12 })
+    ).toBe("cheesecake,6,12,72");
+    expect(generateItemRow({ name: "apple pie", quantity: 5, price: 15 })).toBe(
+      "apple pie,5,15,75"
+    );
+  });
+
+  test("omitting soldout items", () => {
+    expect(generateItemRow({ name: "macaroon", quantity: 0, price: 3 })).toBe(
+      null
+    );
+    expect(
+      generateItemRow({ name: "cheesecake", quantity: 0, price: 12 })
+    ).toBe(null);
+  });
+
+  test("omitting free items", () => {
+    expect(
+      generateItemRow({ name: "plastic cups", quantity: 99, price: 0 })
+    ).toBe(null);
+    expect(generateItemRow({ name: "napkins", quantity: 200, price: 0 })).toBe(
+      null
+    );
+  });
 });
 
-test('omitting soldout items', () => {
-    expect(generateItemRow({ name: "macaroon", quantity: 0, price: 3})).toBe(null);
-    expect(generateItemRow({ name: "cheesecake", quantity: 0, price: 12})).toBe(null);
+describe("generateTotalRow", () => {
+  test("generating a total row", () => {
+    const items = [
+      { name: "apple pie", quantity: 3, price: 15 },
+      { name: "plastic cups", quantity: 0, price: 55 },
+      { name: "macaroon", quantity: 12, price: 3 },
+      { name: "cheesecake", quantity: 0, price: 12 },
+    ];
+
+    expect(generateTotalRow(items)).toBe("Total,,,81");
+    expect(generateTotalRow(items.slice(1))).toBe("Total,,,36");
+    expect(generateTotalRow(items.slice(3))).toBe("Total,,,0");
+  });
 });
 
-test('omitting free items', () => {
-    expect(generateItemRow({name: "plastic cups", quantity: 99, price: 0})).toBe(null);
-    expect(generateItemRow({name: "napkins", quantity: 200, price: 0})).toBe(null);
+describe("createInventoryValuesReport", () => {
+  test("creating reports", () => {
+    const items = [
+      { name: "apple pie", quantity: 3, price: 15 },
+      { name: "cheesecake", quantity: 2, price: 12 },
+      { name: "macaroon", quantity: 20, price: 3 },
+    ];
+    expect(createInventoryValuesReport(items)).toBe(
+      "apple pie,3,15,45\ncheesecake,2,12,24\nmacaroon,20,3,60\nTotal,,,129"
+    );
+    expect(createInventoryValuesReport(items.slice(1))).toBe(
+      "cheesecake,2,12,24\nmacaroon,20,3,60\nTotal,,,84"
+    );
+    expect(createInventoryValuesReport(items.slice(2))).toBe(
+      "macaroon,20,3,60\nTotal,,,60"
+    );
+  });
 });
